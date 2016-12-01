f8256b436ae4c43ca6b05ef016a4fa6a72029376
Brandon <brandonkeithbiggs@gmail.com>
2016-12-01 05:26:33 -0800

    Added readme, and added the template

---
+++ Assets/Scripts/ExampleScript.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+//This is an example script that is attached to an object in the MainScene in the unity editor.
+
+public class ExampleScript : MonoBehaviour
+{
+
+	AudioSource audioSource;
+
+    //Called as soon as this component is created, but before Start.
+    void Awake()
+    {
+        //Debug.Log is like Console.WriteLine, but will print to the Unity console as there is no regular console by default.
+        Debug.Log("Hello world!");
+
+		//Load the sounds
+	var sound = Resources.Load<AudioClip>("Sounds/Death");
+//	var audioSource = gameObject.AddComponent<AudioSource>();
+	audioSource = gameObject.AddComponent<AudioSource>();
+
+	audioSource.clip = sound;
+    }
+    //Called after Awake, but before the first Update.
+    void Start()
+    {
+Camera.main.transform.position = new Vector3(0, 0, -100);
+        }
+
+    //Called every frame.
+    void Update()
+    {
+if(Input.GetKeyDown(KeyCode.Space))
+{
+	audioSource.Play();
+}
+}
+}
+
+class ProjectMain
+{
+[RuntimeInitializeOnLoadMethod]
+public static void Main()
+{
+var obj = new GameObject("TestObj");
+obj.AddComponent<ExampleScript>();
+}
+}
---
+++ Assets/libraries/debugger.cs
@@ -0,0 +1,58 @@
+﻿//Change the variables at the start of the class below to show the path where your logs should be stored
+//Guides on how to make this debugger:
+//http://www.thegameengineer.com/blog/2014/02/09/unity-11-logging/
+//http://www.thegameengineer.com/blog/2014/02/13/unity-12-warnings-errors-and-asserts/
+//note that there is another log file at:
+//C:\Users\userName\AppData\Local\Unity\Editor\Editor.log
+//but it is super detailed and not as easy to read as the ones produced by this script
+using UnityEngine;
+#if UNITY_EDITOR_WIN
+using UnityEditor;
+[InitializeOnLoad]
+#endif
+
+class MyDebugger
+{
+	//set this string to be the place where you wish your logs to be made. It should look something like:
+	//static public string LogPath = "C:/Users/userName/programming/unity/logs/Unity_log.txt";
+	static public string LogPath = "C:/Unity.txt";
+
+	//By default both stack traces and logs will be placed in the file, but if you wish to only have one show, make the other false
+	static readonly bool StackTraceOnLog = true;
+	static public bool ClearLogOnPlay = true;
+
+	static readonly bool Active = true;
+
+	static MyDebugger()
+	{
+		if(Active)
+		{
+			Application.logMessageReceivedThreaded += WriteUnityLog;
+		}
+	}
+
+	[RuntimeInitializeOnLoadMethod(RuntimeInitializeLoadType.BeforeSceneLoad)]
+	static void ClearOnPlay()
+	{
+		if (ClearLogOnPlay)
+		{
+			ClearLog();
+		}
+	}
+
+	public static void ClearLog()
+	{
+		System.IO.File.WriteAllText(LogPath, "");
+	}
+
+	private static void WriteUnityLog(string logString, string stackTrace, LogType type)
+	{
+		
+		System.IO.File.AppendAllText(LogPath, type.ToString() + " : " + logString + '\n');
+		if(type == LogType.Log && !StackTraceOnLog)
+		{
+			return;
+		}
+		System.IO.File.AppendAllText(LogPath, stackTrace + "\n");
+	}
+}
